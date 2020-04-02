d1a47a36d75ff3b65f5955f3ab29bb01ed934952
Pablo Satler <pablosatler@gmail.com>
2020-04-01 23:45:53 -0300

    chore: add named constructors to the button component

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -13,9 +13,9 @@ class Keyboard extends StatelessWidget {
         children: <Widget>[
           ButtonRow(
             [
-              Button(text: 'AC'),
+              Button.big(text: 'AC'),
               Button(text: '%'),
-              Button(text: '/'),
+              Button.operation(text: '/'),
             ],
           ),
           ButtonRow(
@@ -23,7 +23,7 @@ class Keyboard extends StatelessWidget {
               Button(text: '7'),
               Button(text: '8'),
               Button(text: '9'),
-              Button(text: 'x'),
+              Button.operation(text: 'x'),
             ],
           ),
           ButtonRow(
@@ -31,7 +31,7 @@ class Keyboard extends StatelessWidget {
               Button(text: '4'),
               Button(text: '5'),
               Button(text: '6'),
-              Button(text: '-'),
+              Button.operation(text: '-'),
             ],
           ),
           ButtonRow(
@@ -39,14 +39,14 @@ class Keyboard extends StatelessWidget {
               Button(text: '1'),
               Button(text: '2'),
               Button(text: '3'),
-              Button(text: '+'),
+              Button.operation(text: '+'),
             ],
           ),
           ButtonRow(
             [
-              Button(text: '0'),
+              Button.big(text: '0'),
               Button(text: ','),
-              Button(text: '='),
+              Button.operation(text: '='),
             ],
           )
         ],
