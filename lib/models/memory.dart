4419d3dd97a93542b9f2adfc37d4bd9b019b2f3a
Pablo Satler <pablosatler@gmail.com>
2020-04-02 20:29:32 -0300

    fix: adjust logic to eliminate zero being at the left side of numbers

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,5 +1,13 @@
 class Memory {
+  // operations supported by the calculator
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
+
+  // buffer to store the numbers typed by the user
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String operation;
   String _value = '0'; // private variable
+  bool _wipeValue = false;
 
   // getter
   String get value {
@@ -10,11 +18,26 @@ class Memory {
     _value = '0';
   }
 
+  _setOperation(String newOperation) {
+    _wipeValue = true; // to clear the previous typed values of the screen
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    final currentValue = wipeValue ? '' : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+  }
+
   void applyCommand(String command) {
     if(command == 'AC') {
       _allClear();
-      return;
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
+      _addDigit(command); 
     }
-    _value += command;
   }
 }
