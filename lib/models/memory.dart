f027417807bc8dfcad9a0e2db0c1dec1456de403
Pablo Satler <pablosatler@gmail.com>
2020-04-02 20:39:43 -0300

    fix: adjust logic to handle the period (.) being pressed

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -19,16 +19,27 @@ class Memory {
   }
 
   _setOperation(String newOperation) {
-    _wipeValue = true; // to clear the previous typed values of the screen
+    // when you setting a operation, we clear the display
+    _wipeValue = true;
   }
 
   _addDigit(String digit) {
     final isDot = digit == '.';
     final wipeValue = (_value == '0' && !isDot) || _wipeValue;
 
-    final currentValue = wipeValue ? '' : _value;
+    // check if we pressing a period more than one time at the current display
+    if(isDot && _value.contains('.') && !wipeValue) {
+      return;
+    }
+
+    // if wipeValue true, we setting a new operation
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
     _value = currentValue + digit;
     _wipeValue = false;
+
+    // save the value inside the buffer
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   void applyCommand(String command) {
