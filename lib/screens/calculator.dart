f26128c024865ced3580e40ebb9dd98b8d67b3be
Pablo Satler <pablosatler@gmail.com>
2020-04-02 20:01:07 -0300

    chore: set device orientation to portrait up

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../components/display.dart';
 import '../components/keyboard.dart';
@@ -21,6 +22,11 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    // fixing the device orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
       home: Column(
         children: <Widget>[
